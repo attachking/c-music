9c7dd09ef96f5d8e9555096c97d1a49333773308
papachen <447334358@qq.com>
2017-10-28 17:30:20 +0800

    chen

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -6,6 +6,24 @@ const mutations = {
   },
   [types.SET_AUTHOR](state, o) {
     state.author = o
+  },
+  [types.SET_PLAYING](state, o) {
+    state.playing = o
+  },
+  [types.SET_FULLSCREEN](state, o) {
+    state.fullScreen = o
+  },
+  [types.SET_PLAYLIST](state, o) {
+    state.playList = o
+  },
+  [types.SET_SEQUENCELIST](state, o) {
+    state.sequenceList = o
+  },
+  [types.SET_MODE](state, o) {
+    state.mode = o
+  },
+  [types.SET_CURRENTINDEX](state, o) {
+    state.currentIndex = o
   }
 }
 
